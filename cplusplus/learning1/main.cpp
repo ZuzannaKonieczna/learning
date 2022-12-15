fb2615862f7b3f757293daec690c6c9dc6cc1b17
ZuzannaKonieczna <zuzannakonieczna29@gmail.com>
2022-12-15 17:03:28 +0100

    first c++ program

---
+++ cplusplus/learning1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Podaj ilosc uczniow w klasie "<< endl;
+    cin >> a;
+    cout << "Podaj liczbe cukierkow" << endl;
+    cin >> b;
+    cout << "na jednego ucznia przypada " << a/b << endl;
+    return 0;
+}
