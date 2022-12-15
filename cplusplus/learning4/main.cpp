4b35312788017c842fa05a829ce69a044c044876
ZuzannaKonieczna <zuzannakonieczna29@gmail.com>
2022-12-15 19:43:10 +0100

    adding 4 c++ program

---
+++ cplusplus/learning4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    for (int i=15 ;i>=0; i--)
+    {
+
+        cout << i << endl;
+        Sleep (1000);
+       system("cls");
+
+    }
+    cout << "JEBUT" << endl;
+    return 0;
+}
