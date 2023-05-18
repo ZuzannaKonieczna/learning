8b6a5ba6af3038b1a1b46b1cce51f029ef8e18f6
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-05-18 11:44:47 +0200

    warunki

---
+++ phyton/warunki.py
@@ -0,0 +1,8 @@
+l = int(input("Podaj liczbe: "))
+
+if(0<l):
+    print("Liczba jest wieksza od 0")
+elif(0==l):
+    print("Liczba równa 0")
+else:
+    print("liczba mniejsza od 0")
---
+++ phyton/warunki1.py
@@ -0,0 +1,6 @@
+liczba = int(input("Podaj liczbe"))
+
+if (liczba%2==0):
+    print("Liczba jest parzysta ")
+else:
+    print("Liczba jest nieparzyst ")
