8af596ea681020ef671ffef365d2057a59d2d1a1
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-05-21 18:33:59 +0200

    add

---
+++ phyton/temperatura.py
@@ -0,0 +1,9 @@
+celcjusze = tuple(input("Podaj temperature w Celciuszach: "))
+
+pom = celcjusze * 1,8
+farenhtie = pom + 32
+
+print ("Temperatura w Celcjuszach: ")
+print (celcjusze)
+print ("Temperstura w Farenhitie: ")
+print (farenhtie)
