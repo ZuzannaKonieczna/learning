a219772eacd0244208fe31c7afa0a2ae98b5e1f8
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-06-09 12:38:05 +0200

    ex 5

--- phyton/zadanie_5_podstawa.py
+++ phyton/zadanie_5_podstawa.py
@@ -1,3 +1,8 @@
 #Napisz program obliczający obwód i pole prostokąta.
 
-a = int(input("Podaj pierwsxy bok ") )
+a = int(input("Podaj pierwszy bok "))
+b = int(input("Podaj drugi bok "))
+
+print ( "Obwód wynosi: ", 2*a + 2*b)
+print ( "Pole wynosi: " , a*b)
+
