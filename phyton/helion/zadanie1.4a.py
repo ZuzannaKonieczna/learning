e5cd621da2c9609d541204222f332e9d7c8fa2ca
Zuzanna Konieczna <zuzannakonieczna29@gmail.com>
2023-10-26 22:34:26 +0200

    ulepszenia

---
+++ phyton/helion/zadanie1.4a.py
@@ -0,0 +1,15 @@
+x = int(input())
+
+if x<-1: 
+    print ("lol")
+    y = 2*x
+elif(x==1):
+    y = -10
+elif(x==3):
+    y = x **4
+elif(x==6):
+    y = (x - 4) ** (1/2)
+else:
+    y = 0
+
+print (y)
